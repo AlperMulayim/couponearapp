59fda257ae6b1f0fa2975308ea4d52b8b482b79b
alpermulayim <alper.mulayim48@gmail.com>
2022-09-07 21:47:44 +0300

    init project

---
+++ couponear/src/main/java/com/alper/couponear/CouponearApplication.java
@@ -0,0 +1,13 @@
+package com.alper.couponear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponearApplication.class, args);
+	}
+
+}
---
+++ couponear/src/test/java/com/alper/couponear/CouponearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alper.couponear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
