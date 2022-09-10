82b22547c8876491899d2d63b6f544b594dc34e2
alpermulayim <alper.mulayim48@gmail.com>
2022-09-10 18:08:54 +0300

    Swagger Documentation Added.

--- couponear/src/main/java/com/alper/couponear/CouponearApplication.java
+++ couponear/src/main/java/com/alper/couponear/CouponearApplication.java
@@ -2,12 +2,31 @@ package com.alper.couponear;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class CouponearApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CouponearApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()                 .apis(RequestHandlerSelectors.basePackage("com.alper"))
+				.paths(regex("/api.*"))
+				.build();
+
 	}
 
 }
