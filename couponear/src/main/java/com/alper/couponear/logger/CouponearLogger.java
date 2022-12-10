086e4d5e417fb074e175e9a7ce379fda13cbd0a2
alpermulayim <alper.mulayim48@gmail.com>
2022-12-10 16:28:08 +0300

    Spring AOP Logging added for Campaign Controller

--- couponear/src/main/java/com/alper/couponear/logger/CouponearLogger.java
+++ couponear/src/main/java/com/alper/couponear/logger/CouponearLogger.java
@@ -12,16 +12,24 @@ import java.lang.reflect.Method;
 @Aspect
 @Component
 public class CouponearLogger {
-    private static  final String COTROLLERS_POINT_CUT = "within(com.alper.couponear.campaing.CampaignController)";
+    private static  final String POINT_CUT = "within(com.alper.couponear..*)";
 
-    @Around(COTROLLERS_POINT_CUT)
-    public Object logExec(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        Signature signature = proceedingJoinPoint.getSignature();
-        MethodSignature methodSignature = (MethodSignature) signature;
-        Method method = methodSignature.getMethod();
-        System.out.println("BEFORE- {} - " + method.getName());
-        var proceed =  proceedingJoinPoint.proceed();
-        System.out.println("AFTER - {} - " + proceed.toString());
+    @Around(POINT_CUT)
+    public Object logExec(ProceedingJoinPoint proceedingJoinPoint) {
+        Object proceed = null;
+
+        try {
+            Signature signature = proceedingJoinPoint.getSignature();
+            MethodSignature methodSignature = (MethodSignature) signature;
+            Method method = methodSignature.getMethod();
+            proceed =  proceedingJoinPoint.proceed();
+            System.out.println("[ METHOD_EXECUTED ]-- " + method  + "  -- [ METHOD_RESULT ]-- " + proceed.toString());
+        }catch (Exception e){
+            System.out.println("[ LOG_ERROR ]-- Method Logging Error when execution process" + e);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+
         return proceed;
     }
 }
